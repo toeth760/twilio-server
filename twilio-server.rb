2f0a937e2664bd4d1e35398698f11b2fc287cb1b
Tyler Oeth <toeth760@gmail.com>
2014-08-05 14:07:50 -0700

    now has packagfed gemfiles

--- twilio-server.rb
+++ twilio-server.rb
@@ -1,14 +1,13 @@
-# require 'twilio-ruby'
+require 'twilio-ruby'
 require 'sinatra'
-# require 'curl'
-# require 'csv'
-
+require 'curl'
+require 'csv'
 #require 'httpclient' => sinatra require does not lke this require for some reason... find out why (need this for redirects)
 #require 'uri'
 #require 'rubygems'
 
-#set :port, 4567
-# set :c, 0
+# set :port, 4567
+set :c, 0
 
 ###get list of urls from csv file
 
@@ -220,41 +219,36 @@ url_count = urls.count
 
 ###check url for redirects
 
-# def getredirectedurl(url)
-# 	result = Curl::Easy.perform(url) do |curl| 
-# 	  curl.headers["User-Agent"] = "..."
-# 	  curl.verbose = false
-# 	  curl.follow_location = true
-# 	end
-# 	return result.last_effective_url
-# end
-
-get '/' do
-  puts urls
-  puts url_count
+def getredirectedurl(url)
+	result = Curl::Easy.perform(url) do |curl| 
+	  curl.headers["User-Agent"] = "..."
+	  curl.verbose = false
+	  curl.follow_location = true
+	end
+	return result.last_effective_url
 end
 
 ###sinatra get request handling
 
-# get '/' do
-# 	pass if request.path_info == "/favicon.ico"
-# 	pass if settings.c >= url_count
-# 	Twilio::TwiML::Response.new do |r|
-# 	    r.Say getredirectedurl(urls[settings.c])
-# 	end.text
-# end
+get %r{/.*} do
+	pass if request.path_info == "/favicon.ico"
+	pass if settings.c >= url_count
+	Twilio::TwiML::Response.new do |r|
+	    r.Say getredirectedurl(urls[settings.c])
+	end.text
+end
 
-# get '/' do
-# 	pass if request.path_info == "/favicon.ico"
-# 	pass if settings.c < url_count
-# 	"no more files!"
-# end
+get %r{/.*} do
+	pass if request.path_info == "/favicon.ico"
+	pass if settings.c < url_count
+	"no more files!"
+end
 
-# after do
-# 	if settings.c < url_count && request.path_info != "/favicon.ico"
-# 		settings.c += 1
-# 		puts "sending file #{settings.c} of #{url_count}"
-# 	else
-# 		puts "no files to send!"
-# 	end
-# end
+after do
+	if settings.c < url_count && request.path_info != "/favicon.ico"
+		settings.c += 1
+		puts "sending file #{settings.c} of #{url_count}"
+	else
+		puts "no files to send!"
+	end
+end
