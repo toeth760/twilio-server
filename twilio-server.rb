244c9ec3347caefefcc66c359b3b1261f72fd2a3
Tyler Oeth <toeth760@gmail.com>
2014-08-06 10:59:03 -0700

    working!

--- twilio-server.rb
+++ twilio-server.rb
@@ -6,13 +6,16 @@ require 'csv'
 set :c, 0
 
 ###get urls from csv file
-
-csv_fname = "callinfo.csv"
-csv = CSV.read( csv_fname, :headers=>true)
-urls = csv['call_recording_url']
-urls = urls.uniq
-url_count = urls.length
-puts "$mark$ csv was read, there are #{url_count} urls in the list. Should be 204"
+begin
+	csv_fname = "callinfo.csv"
+	csv = CSV.read( csv_fname, :headers=>true)
+	urls = csv['call_recording_url']
+	urls = urls.uniq
+	url_count = urls.length
+	puts "$mark$ csv was read, there are #{url_count} urls in the list. Should be 204"
+	rescue Exception => msg
+	puts msg
+end
 
 ###check url for redirects
 
