f3049d42c579af65508287cadb9cc56ca69b85d8
Tyler Oeth <toeth760@gmail.com>
2014-08-06 11:53:02 -0700

    working!

--- twilio-server.rb
+++ twilio-server.rb
@@ -6,17 +6,24 @@ require 'csv'
 set :c, 0
 
 ###get urls from csv file
-begin
-	csv_fname = "callinfo.csv"
-	csv = CSV.read( csv_fname, :headers=>true)
-	urls = csv['call_recording_url']
-	urls = urls.uniq
-	url_count = urls.length
-	puts "$mark$ csv was read, there are #{url_count} urls in the list. Should be 204"
-	rescue Exception => msg
-	"$mark$ had this error: #{msg}"
-	puts msg
-end
+# begin
+# 	csv_fname = "callinfo.csv"
+# 	csv = CSV.read( csv_fname, :headers=>true)
+# 	urls = csv['call_recording_url']
+# 	urls = urls.uniq
+# 	url_count = urls.length
+# 	puts "$mark$ csv was read, there are #{url_count} urls in the list. Should be 204"
+# 	rescue Exception => msg
+# 	"$mark$ had this error: #{msg}"
+# 	puts msg
+# end
+
+urls = ["https://calltrackdata.com/webreports/audio.jsp?callID=2086701093&authentication=75E0E52C2F022233FC3070FC979C7E33",
+"https://calltrackdata.com/webreports/audio.jsp?callID=2086725818&authentication=21C006EED2356B4A1D796F0DE957C6DA",
+"https://calltrackdata.com/webreports/audio.jsp?callID=44609277&authentication=5D46E05C2DF139E6C4C47F16206287C1",
+"https://calltrackdata.com/webreports/audio.jsp?callID=2086781467&authentication=E83484B24EC788F446EABC7F6B4049A0",
+"https://calltrackdata.com/webreports/audio.jsp?callID=2086796602&authentication=3B37BCD861F88B4F8D4366E3004370B5"]
+
 
 ###check url for redirects
 
